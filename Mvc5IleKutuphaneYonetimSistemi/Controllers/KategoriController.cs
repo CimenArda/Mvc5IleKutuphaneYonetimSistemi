43d3312bbe4b1d0037d341eb62cebb4812ab8ee7
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-04 15:21:36 +0300

    Kategori Entitysi CRUD işlemleri

---
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/KategoriController.cs
@@ -0,0 +1,80 @@
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+        
+        public ActionResult Index()
+        {
+            var kategoriler = db.TblKategori.ToList();
+
+            return View(kategoriler);
+        }
+
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TblKategori kategori)
+        {
+
+            if (kategori != null && !string.IsNullOrWhiteSpace(kategori.AD))
+            {
+                db.TblKategori.Add(kategori);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Error = "Kategori adı boş olamaz!";
+
+                return View();
+            }
+        }
+
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategoribul =db.TblKategori.Find(id);
+          
+                db.TblKategori.Remove(kategoribul);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+        }
+
+
+
+
+        [HttpGet]
+        public ActionResult KategoriGuncelle(int id)
+        {
+            var kategoribul = db.TblKategori.Find(id);
+
+            return View("KategoriGuncelle", kategoribul);
+        }
+
+
+
+        [HttpPost]
+        public ActionResult KategoriGuncelle(TblKategori kategori)
+        {
+            var ktg = db.TblKategori.Find(kategori.ID);
+            ktg.AD = kategori.AD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
