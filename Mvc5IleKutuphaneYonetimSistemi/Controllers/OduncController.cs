29a0474d28382d5fb5fe45afc20c0568049c5bea
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-06 13:01:00 +0300

    Ödünç& Emnater Kitap İşlemleri-1

---
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/OduncController.cs
@@ -0,0 +1,52 @@
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
+{
+    public class OduncController : Controller
+    {
+        // GET: Odunc
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+
+        public ActionResult Index()
+        {
+            var hareketler = db.TblHareket.ToList();
+
+            return View(hareketler);
+        }
+
+        public ActionResult OduncVer()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OduncVer(TblHareket hareket)
+        {
+
+            if (hareket != null && !string.IsNullOrWhiteSpace(hareket.KITAP.ToString()) && !string.IsNullOrWhiteSpace(hareket.PERSONEL.ToString()))
+            {
+                db.TblHareket.Add(hareket);
+                db.SaveChanges();
+                return View();
+            }
+            else
+            {
+                ViewBag.Error = "Hareket Değerleri boş olamaz!";
+                ViewBag.Error2 = "Hareket Değerleri boş olamaz!";
+
+                return View();
+            }
+        }
+
+
+
+
+
+    }
+}
