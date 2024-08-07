29d5608af993459a5faa6e4a0ee8b06193a88723
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-07 13:56:58 +0300

    Dashboard Kısmı Tasarım İşlemleri

---
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/IstatistikController.cs
@@ -0,0 +1,20 @@
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        // GET: Istatistik
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
