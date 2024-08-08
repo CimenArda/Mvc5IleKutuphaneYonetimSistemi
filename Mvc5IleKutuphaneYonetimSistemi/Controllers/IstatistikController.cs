dbf5e96975443cd755711f5281c0d44d6f7e971a
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-08 14:21:48 +0300

    Widged Kart Oluşturulup Linq Sorguları İle Kullanılması

--- Mvc5IleKutuphaneYonetimSistemi/Controllers/IstatistikController.cs
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/IstatistikController.cs
@@ -15,6 +15,14 @@ namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
 
         public ActionResult Index()
         {
+         
+            ViewBag.toplamuye =db.TblUye.Count().ToString();
+            ViewBag.toplamkitap =db.TblKitap.Count().ToString();
+
+            ViewBag.emanetkitap =db.TblKitap.Where(x=>x.DURUM==false).Count().ToString();
+
+            ViewBag.kasa =db.TblCeza.Sum(x=>x.PARA).ToString();
+            
             return View();
         }
 
@@ -47,6 +55,36 @@ namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
             }
 
             return RedirectToAction("Galeri");
+        }
+
+
+
+        public ActionResult LinqKart()
+        {
+            ViewBag.dgr1 = db.TblKitap.Count().ToString();
+            ViewBag.dgr2 = db.TblUye.Count().ToString();
+            ViewBag.dgr3 = db.TblCeza.Sum(x=>x.PARA).ToString();
+            ViewBag.dgr4 = db.TblKitap.Where(x=>x.DURUM==false).Count().ToString();
+            ViewBag.dgr5= db.TblKategori.Count().ToString();
+
+
+            ViewBag.dgr8 = db.enFazlakitapYazar().FirstOrDefault();
+
+
+            ViewBag.dgr9 = db.TblKitap.GroupBy(x => x.YAYINEVİ).OrderByDescending(z => z.Count()).Select(y => new { y.Key }).FirstOrDefault();
+
+            ViewBag.dgr11= db.TblIletisim.Count().ToString();
+
+
+
+
+
+
+
+
+
+
+            return View();
         }
     }
 }
