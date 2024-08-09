b637295971044745b3ef4f465dda6542acf41dd6
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-09 12:50:53 +0300

    Login İşlemleri

--- Mvc5IleKutuphaneYonetimSistemi/Controllers/LoginController.cs
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/LoginController.cs
@@ -1,17 +1,54 @@
-﻿using System;
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(TblUye p)
+        {
+            var BİLGİLER =db.TblUye.FirstOrDefault(x=>x.MAIL==p.MAIL && x.SIFRE==p.SIFRE);
+
+
+            if (BİLGİLER !=null)
+            {
+                FormsAuthentication.SetAuthCookie(BİLGİLER.MAIL, false);
+
+                Session["Mail"] = BİLGİLER.MAIL.ToString();
+
+                //Session["Ad"] = BİLGİLER.AD.ToString();
+                //Session["Soyad"] = BİLGİLER.SOYAD.ToString();
+                //Session["KullanıcıAd"] = BİLGİLER.KULLANICIAD.ToString();
+                //Session["Okul"] = BİLGİLER.OKUL.ToString();
+                //Session["Sifre"] = BİLGİLER.SIFRE.ToString();
+                //Session["Id"] = BİLGİLER.ID.ToString();
+
+
+                return RedirectToAction("Index2", "Panelim");
+
+            }
+           return View();
+
+
+        }
+
+
+
+
+
     }
 }
--- Mvc5IleKutuphaneYonetimSistemi/Controllers/PanelimController.cs
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,34 @@ namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
 {
     public class PanelimController : Controller
     {
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+
         // GET: Panelim
-        public ActionResult Index()
+        [Authorize]
+        public ActionResult Index2()
         {
+            var uyeMail = (string)Session["Mail"];
+            var degerler =db.TblUye.FirstOrDefault(z=>z.MAIL == uyeMail);
+
+            return View(degerler);
+        }
+
+
+
+
+        [HttpPost]
+        public ActionResult Index2(TblUye p)
+        {
+
+            var kullanici = (string)Session["Mail"];
+            var uye =db.TblUye.FirstOrDefault(x=>x.MAIL ==kullanici);
+
+            uye.SIFRE = p.SIFRE;
+            uye.AD = p.AD;
+            uye.SOYAD = p.SOYAD;
+            uye.KULLANICIAD = p.KULLANICIAD;
+            uye.OKUL = p.OKUL;
+            db.SaveChanges();
             return View();
         }
     }
