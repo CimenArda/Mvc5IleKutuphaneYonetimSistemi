5065b8eb5220c9d48162bc9f8f8495b9d6d9a79e
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-09 11:14:53 +0300

    Login,Register,Öğrenci Paneli templatelerinin entegre edilmesi

---
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/RegisterController.cs
@@ -0,0 +1,33 @@
+﻿using Mvc5IleKutuphaneYonetimSistemi.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        DbKutuphaneEntities db = new DbKutuphaneEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(TblUye uye)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+
+            }
+            db.TblUye.Add(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index","Login");
+        }
+    }
+}
