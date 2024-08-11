869fdb483d4574309cabfef6d53c9c1c09793b54
ArdaCimen <cimenarda.yaz@gmail.com>
2024-08-11 15:01:00 +0300

    Geriye Dönük Düzenlemeler-Kitap Ödünç Sayfasını Dinamik Hale Getirme

--- Mvc5IleKutuphaneYonetimSistemi/Controllers/OduncController.cs
+++ Mvc5IleKutuphaneYonetimSistemi/Controllers/OduncController.cs
@@ -21,28 +21,65 @@ namespace Mvc5IleKutuphaneYonetimSistemi.Controllers
 
         public ActionResult OduncVer()
         {
+            List<SelectListItem> deger1 = (from x in db.TblUye.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.AD +" " +x.SOYAD,
+                                               Value = x.ID.ToString()
+                                           }).ToList();
+            ViewBag.deger1 = deger1;
 
+
+            List<SelectListItem> deger2 = (from x in db.TblKitap.Where(x=>x.DURUM==true).ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.AD,
+                                               Value = x.ID.ToString()
+                                           }).ToList();
+            ViewBag.deger2 = deger2;
+            List<SelectListItem> deger3 = (from x in db.TblPersonel.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.PERSONEL,
+                                               Value = x.ID.ToString()
+                                           }).ToList();
+            ViewBag.deger3 = deger3;
             return View();
         }
 
         [HttpPost]
         public ActionResult OduncVer(TblHareket hareket)
         {
-
-            if (hareket != null && !string.IsNullOrWhiteSpace(hareket.KITAP.ToString()) && !string.IsNullOrWhiteSpace(hareket.PERSONEL.ToString()))
+            if (hareket != null && hareket.TblUye != null && hareket.TblKitap != null && hareket.TblPersonel != null)
             {
-                db.TblHareket.Add(hareket);
-                db.SaveChanges();
-                return View();
+                var uye = db.TblUye.Find(hareket.TblUye.ID);
+                var kitap = db.TblKitap.Find(hareket.TblKitap.ID);
+                var personel = db.TblPersonel.Find(hareket.TblPersonel.ID);
+
+                if (uye != null && kitap != null && personel != null)
+                {
+                    hareket.TblUye = uye;
+                    hareket.TblKitap = kitap;
+                    hareket.TblPersonel = personel;
+
+                    db.TblHareket.Add(hareket);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "Geçerli bir Üye, Kitap veya Personel seçiniz.";
+                }
             }
             else
             {
                 ViewBag.Error = "Hareket Değerleri boş olamaz!";
-                ViewBag.Error2 = "Hareket Değerleri boş olamaz!";
-
-                return View();
             }
+
+            return View(hareket);
         }
+
 
         public ActionResult IadeEt(TblHareket p)
         {
